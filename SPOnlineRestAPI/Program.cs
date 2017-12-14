acbf8ed174cd1f8857a1a2e2d22c333d68da54c4
Steve Yu <steve.ykyu@gmail.com>
2017-12-14 18:30:31 +0800

    RestSPClient
    This is the initial client

---
+++ SPOnlineRestAPI/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using System.Security;
+using Microsoft.SharePoint.Client;
+using SPOnlineRestAPI;
+
+namespace SPOnlineRestAPI
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Uri myUri = new Uri("https://pttlab.sharepoint.com/sites/ScanDemo/", UriKind.Absolute);
+			string sUsername = "steveyu2@pttlab.onmicrosoft.com";
+			string sPassword = "P@ssw0rd1234";
+			string sDocLib = "Documents";
+
+			//GetListItems(myUri, sUsername, sPassword,sDocLib);
+			//GetListItemByID(myUri, sUsername, sPassword, sDocLib, "37");
+			UploadDoc(myUri, sUsername, sPassword, sDocLib);
+		}
+
+		public static void GetListItems(Uri webUri,string userName,string password,string LstTitle)
+		{
+			using (var client = new SPHttpClient(webUri, userName, password))
+			{
+				var listTitle = LstTitle;
+				var endpointUrl = string.Format("{0}/_api/web/lists/getbytitle('{1}')/items", webUri, listTitle);
+				//The JSON Result
+				var data = client.ExecuteJson(endpointUrl);
+				//foreach (var item in data["Value"])
+				//{
+				//	Console.WriteLine(item["Title"]);
+				//}
+			}
+		}
+
+		public static void GetListItemByID(Uri webUri, string userName, string password, string LstTitle,string itemId)
+		{
+			using (var client = new SPHttpClient(webUri, userName, password))
+			{
+				//var listTitle = "Tasks";
+				//var itemId = 1;
+				var endpointUrl = string.Format("{0}/_api/web/lists/getbytitle('{1}')/items({2})", webUri, LstTitle, itemId);
+				var data = client.ExecuteJson(endpointUrl);
+				//Console.WriteLine(data["Title"]);
+			}
+		}
+
+		public static void CreateDoc(Uri webUri, string userName, string password, string LstTitle)
+		{
+			using (var client = new SPHttpClient(webUri, userName, password))
+			{
+				var passWord = new SecureString();
+				foreach (var c in password) passWord.AppendChar(c);
+				SharePointOnlineCredentials credential = new SharePointOnlineCredentials(userName, passWord);
+
+				//byte[] bytefile = System.IO.File.ReadAllBytes(pathToFile);
+				string siteurl = webUri.ToString();
+				string documentlibrary = LstTitle; //Document library where file needs to be uploaded
+
+				string filePath = @"C:\temp\test.docx";
+
+				byte[] binary = System.IO.File.ReadAllBytes(filePath);
+				string fname = System.IO.Path.GetFileName(filePath);
+				string result = string.Empty;
+				//Url to upload file
+				string resourceUrl = string.Format("{0}/_api",siteurl);
+
+				RestClient RC = new RestClient(resourceUrl);
+				NetworkCredential NCredential = System.Net.CredentialCache.DefaultNetworkCredentials;
+				//client.c
+				RC.Authenticator = new NtlmAuthenticator(NCredential);
+
+				Console.WriteLine("Creating Rest Request");
+
+				RestRequest Request = new RestRequest("contextinfo ?$select = FormDigestValue", Method.POST);
+				Request.AddHeader("Accept", "application/json; odata = verbose");
+				Request.AddHeader("Body", "");
+
+				string ReturnedStr = RC.Execute(Request).Content;
+				int StartPos = ReturnedStr.IndexOf("FormDigestValue") + 18;
+				int length = ReturnedStr.IndexOf(@""",", StartPos)-StartPos;
+				string FormDigestValue = ReturnedStr.Substring(StartPos, length);
+
+				Console.WriteLine("Uploading file Site……");
+
+				resourceUrl = string.Format("/web/GetFolderByServerRelativeUrl(‘{ 0}’)/ Files / add(url = '{1}’,overwrite=true)", documentlibrary ,fname);
+					Request = new RestRequest(resourceUrl, Method.POST);
+				Request.RequestFormat = DataFormat.Json;
+				Request.AddHeader("Accept", "application/json; odata = verbose");
+				Request.AddHeader("X - RequestDigest", FormDigestValue);
+				Console.WriteLine("File is successfully uploaded to sharepoint site.");
+				Console.ReadLine();
+
+			}
+		}
+
+		
+		public static void UploadDoc(Uri webUri, string userName, string password, string LstTitle)
+		{
+			try
+			{
+				const string DOCNAME = @"c:\temp\test.docx";
+				const string FOLDERURL = "Documents";
+
+				//Reading document from file system
+				System.IO.MemoryStream doc = new System.IO.MemoryStream(System.IO.File.ReadAllBytes(DOCNAME));
+
+				//Uploading document
+				
+				var t = SPUploadClient.uploadDocumentAsync(webUri.ToString(), userName, password, doc, FOLDERURL, System.IO.Path.GetFileName(DOCNAME));
+				t.Wait();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+			}
+			Console.ReadLine();
+		}
+
+
+
+	}
+}
---
+++ SPOnlineRestAPI/SPUploadClient.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.SharePoint.Client;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPOnlineRestAPI
+{
+	public class SPUploadClient
+	{
+		/// <summary>
+		/// Return Form Digest information
+		/// </summary>
+		/// <param name="handler"></param>
+		/// <param name="webUrl"></param>
+		/// <returns></returns>
+		private static async Task<Models.FormDigestInfo.Rootobject> GetFormDigest(HttpClientHandler handler, string webUrl)
+		{
+			//Creating REST url to get Form Digest
+			const string RESTURL = "{0}/_api/contextinfo";
+			string restUrl = string.Format(RESTURL, webUrl);
+
+			//Adding headers
+			var client = new HttpClient(handler);
+			client.DefaultRequestHeaders.Accept.Clear();
+			client.DefaultRequestHeaders.Add("Accept", "application/json;odata=nometadata");
+
+			//Perform call
+			HttpResponseMessage response = await client.PostAsync(restUrl, null).ConfigureAwait(false);
+			response.EnsureSuccessStatusCode();
+
+			//Reading string data
+			string jsonData = await response.Content.ReadAsStringAsync();
+
+			//Creating FormDigest object
+			Models.FormDigestInfo.Rootobject res = JsonConvert.DeserializeObject<Models.FormDigestInfo.Rootobject>(jsonData);
+			return res;
+		}
+
+		/// <summary>
+		/// Upload a document
+		/// </summary>
+		/// <param name="webUrl"></param>
+		/// <param name="loginName"></param>
+		/// <param name="pwd"></param>
+		/// <param name="document"></param>
+		/// <param name="folderServerRelativeUrl"></param>
+		/// <param name="fileName"></param>
+		/// <returns></returns>
+		public static async Task uploadDocumentAsync(string webUrl, string loginName, string pwd, System.IO.MemoryStream document, string folderServerRelativeUrl, string fileName)
+		{
+			try
+			{
+				//Creating credentials
+				var passWord = new SecureString();
+				foreach (var c in pwd) passWord.AppendChar(c);
+				SharePointOnlineCredentials credential = new SharePointOnlineCredentials(loginName, passWord);
+
+				//Creating REST url
+				const string RESTURL = "{0}/_api/web/GetFolderByServerRelativeUrl('{1}')/Files/add(url='{2}',overwrite=true)";
+				string rESTUrl = string.Format(RESTURL, webUrl, folderServerRelativeUrl, fileName);
+
+				//Creating handler
+				using (var handler = new HttpClientHandler() { Credentials = credential })
+				{
+					//Getting authentication cookies
+					Uri uri = new Uri(webUrl);
+					handler.CookieContainer.SetCookies(uri, credential.GetAuthenticationCookie(uri));
+
+					//Getting form digest
+					var tFormDigest = GetFormDigest(handler, webUrl);
+					tFormDigest.Wait();
+
+					//Creating HTTP Client
+					using (var client = new HttpClient(handler))
+					{
+						client.DefaultRequestHeaders.Accept.Clear();
+						client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+						client.DefaultRequestHeaders.Add("Accept", "application/json;odata=nometadata");
+						client.DefaultRequestHeaders.Add("binaryStringRequestBody", "true");
+						client.DefaultRequestHeaders.Add("X-RequestDigest", tFormDigest.Result.FormDigestValue);
+						client.MaxResponseContentBufferSize = 2147483647;
+
+						//Creating Content
+						ByteArrayContent content = new ByteArrayContent(document.ToArray());
+
+						//Perform post
+						HttpResponseMessage response = await client.PostAsync(rESTUrl, content).ConfigureAwait(false);
+
+						//Ensure 200 (Ok)
+						response.EnsureSuccessStatusCode();
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				throw new ApplicationException($"Error uploading document {fileName} call on folder {folderServerRelativeUrl}. {ex.Message}", ex);
+			}
+		}
+	}
+
+	public class Models
+	{
+		public class FormDigestInfo
+		{
+			public class Rootobject
+			{
+				public int FormDigestTimeoutSeconds { get; set; }
+				public string FormDigestValue { get; set; }
+				public string LibraryVersion { get; set; }
+				public string SiteFullUrl { get; set; }
+				public string[] SupportedSchemaVersions { get; set; }
+				public string WebFullUrl { get; set; }
+			}
+		}
+	}
+}
+
